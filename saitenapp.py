d13a3ef87e7ba5cd543dc2c1c9fc54e88c286b4a
shinjimec <shinjimec@gmail.com>
2025-02-10 08:38:36 +0900

    Add files via upload

--- saitenapp.py
+++ saitenapp.py
@@ -3,7 +3,7 @@ from PIL import Image
 import pandas as pd
 
 st.title('採点サービス　ログイン者数推移')
-st.caption('これは採点サービスのログイン者数の推移グラフです。2025年1月24日(金)～稼働。')
+st.caption('★採点サービスのログイン者数の推移グラフです。2025年1月24日(金)～稼働。')
 
 # 画像
 image = Image.open('mec_saiten.jpg')
@@ -12,4 +12,9 @@ st.image(image,width=250)
 # データ分析
 df = pd.read_csv('saiten.csv',index_col='日')
 st.line_chart(df)
-st.dataframe(df.style.highlight_max(axis=0))
+st.dataframe(df.style.highlight_max(axis=0), height=200)
+
+st.caption('★採点サービスの大学別の入力状況です。2025年2月9日時点')
+# データ分析
+df2 = pd.read_excel('saitenuni.xlsx',index_col='大学', dtype='object')
+st.dataframe(df2, width=600, height=3250)
